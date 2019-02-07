5ac3e4c0cd3e591a6c6cbcdf89812a58e946960a
Tim <tim.medcalf@pocketworks.co.uk>
2019-02-07 08:55:32 +0000

    TM | Removing unecessary comments

--- Applozic/ViewControllers/ALBaseViewController.m
+++ Applozic/ViewControllers/ALBaseViewController.m
@@ -373,15 +373,6 @@ static CGFloat const sendTextViewCornerRadius = 15.0f;
 {
     NSString * theAnimationDuration = [self handleKeyboardNotification:notification];
 
-//OLD
-//    if (@available(iOS 11.0, *)) {
-//        self.checkBottomConstraint.constant = self.view.frame.size.height - keyboardEndFrame.origin.y + navigationWidth - self.view.safeAreaInsets.bottom;
-//    } else {
-//        // Fallback on earlier versions
-//        self.checkBottomConstraint.constant = self.view.frame.size.height - keyboardEndFrame.origin.y + navigationWidth;
-//    }
-
-//NEW
     self.checkBottomConstraint.constant = -1 * keyboardEndFrame.size.height +
     self.bottomLayoutGuide.length;
 
